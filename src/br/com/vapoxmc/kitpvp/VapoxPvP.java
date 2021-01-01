62d363cbb37568c785de11146cf6f1e6f6459334
crlshnrrq <caarlosheenrique01@outlook.com>
2021-01-01 19:06:07 -0300

    Added Anchor kit

--- src/br/com/vapoxmc/kitpvp/VapoxPvP.java
+++ src/br/com/vapoxmc/kitpvp/VapoxPvP.java
@@ -9,6 +9,7 @@ import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import br.com.vapoxmc.kitpvp.kit.Ajnin;
+import br.com.vapoxmc.kitpvp.kit.Anchor;
 import br.com.vapoxmc.kitpvp.kit.Kangaroo;
 import br.com.vapoxmc.kitpvp.kit.Kit;
 import br.com.vapoxmc.kitpvp.kit.Ninja;
@@ -47,6 +48,7 @@ public final class VapoxPvP extends JavaPlugin {
 		getKits().clear();
 		getKits().add(defaultKit = new PvP());
 		getKits().add(new Ajnin());
+		getKits().add(new Anchor());
 		getKits().add(new Kangaroo());
 		getKits().add(new Ninja());
 		getKits().add(new Stomper());
---
+++ src/br/com/vapoxmc/kitpvp/kit/Anchor.java
@@ -0,0 +1,12 @@
+package br.com.vapoxmc.kitpvp.kit;
+
+import org.bukkit.Material;
+
+import br.com.vapoxmc.kitpvp.utils.Stack;
+
+public final class Anchor extends Kit {
+
+	public Anchor() {
+		super("Anchor", "Não tome e nem dê repulsão!", new Stack(Material.ANVIL));
+	}
+}
