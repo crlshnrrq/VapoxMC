0b663b98c34a6dd50ccdb2515388eec8bcb109e9
crlshnrrq <caarlosheenrique01@outlook.com>
2021-01-01 19:23:09 -0300

    Added Urgal kit

--- src/br/com/vapoxmc/kitpvp/VapoxPvP.java
+++ src/br/com/vapoxmc/kitpvp/VapoxPvP.java
@@ -19,6 +19,7 @@ import br.com.vapoxmc.kitpvp.kit.PvP;
 import br.com.vapoxmc.kitpvp.kit.Snail;
 import br.com.vapoxmc.kitpvp.kit.Stomper;
 import br.com.vapoxmc.kitpvp.kit.Thor;
+import br.com.vapoxmc.kitpvp.kit.Urgal;
 import br.com.vapoxmc.kitpvp.kit.Viking;
 import br.com.vapoxmc.kitpvp.kit.Viper;
 import br.com.vapoxmc.kitpvp.utils.Strings;
@@ -62,6 +63,7 @@ public final class VapoxPvP extends JavaPlugin {
 		getKits().add(new Snail());
 		getKits().add(new Stomper());
 		getKits().add(new Thor());
+		getKits().add(new Urgal());
 		getKits().add(new Viking());
 		getKits().add(new Viper());
 
---
+++ src/br/com/vapoxmc/kitpvp/kit/Urgal.java
@@ -0,0 +1,12 @@
+package br.com.vapoxmc.kitpvp.kit;
+
+import org.bukkit.Material;
+
+import br.com.vapoxmc.kitpvp.utils.Stack;
+
+public final class Urgal extends Kit {
+
+	public Urgal() {
+		super("Urgal", "Aumente sua força com a poção", new Stack(Material.RED_ROSE));
+	}
+}
