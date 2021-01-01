bf31d703552480715da5952565031c874c56d619
crlshnrrq <caarlosheenrique01@outlook.com>
2021-01-01 18:52:54 -0300

    Added PvP kit

---
+++ src/br/com/vapoxmc/kitpvp/kit/PvP.java
@@ -0,0 +1,12 @@
+package br.com.vapoxmc.kitpvp.kit;
+
+import org.bukkit.Material;
+
+import br.com.vapoxmc.kitpvp.utils.Stack;
+
+public final class PvP extends Kit {
+
+	public PvP() {
+		super("PvP", "Kit sem abilidades, com uma espada afiada!", new Stack(Material.STONE_SWORD));
+	}
+}
