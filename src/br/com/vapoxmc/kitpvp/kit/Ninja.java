b702524d6e7e064c2b06d98b1e0b39a054494401
crlshnrrq <caarlosheenrique01@outlook.com>
2021-01-01 19:01:32 -0300

    Added Ninja kit

---
+++ src/br/com/vapoxmc/kitpvp/kit/Ninja.java
@@ -0,0 +1,12 @@
+package br.com.vapoxmc.kitpvp.kit;
+
+import org.bukkit.Material;
+
+import br.com.vapoxmc.kitpvp.utils.Stack;
+
+public final class Ninja extends Kit {
+
+	public Ninja() {
+		super("Ninja", "Teleporte-se para seu oponente!", new Stack(Material.ENDER_PEARL));
+	}
+}
