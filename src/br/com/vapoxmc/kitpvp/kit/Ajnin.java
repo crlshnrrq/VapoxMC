4c2330ed6d612efd49ac4ec2a48acc56b5302132
crlshnrrq <caarlosheenrique01@outlook.com>
2021-01-02 10:29:58 -0300

    Added Ajnin ability

--- src/br/com/vapoxmc/kitpvp/kit/Ajnin.java
+++ src/br/com/vapoxmc/kitpvp/kit/Ajnin.java
@@ -1,12 +1,70 @@
 package br.com.vapoxmc.kitpvp.kit;
 
-import org.bukkit.Material;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.player.PlayerToggleSneakEvent;
+
+import br.com.vapoxmc.kitpvp.VapoxPvP;
 import br.com.vapoxmc.kitpvp.utils.Stack;
 
-public final class Ajnin extends Kit {
+public final class Ajnin extends Kit implements Listener {
+
+	public static final Map<UUID, UUID> tegratMap = new HashMap<>();
 
 	public Ajnin() {
 		super("Ajnin", "Teleporte seu oponente até você!", new Stack(Material.NETHER_STAR));
+	}
+
+	@EventHandler
+	private void onEntityDamamgeByEntity(EntityDamageByEntityEvent event) {
+		if (event.getEntity() instanceof Player && event.getDamager() instanceof Player) {
+			Player player = (Player) event.getEntity(), damager = (Player) event.getDamager();
+			if (VapoxPvP.getKit(damager) instanceof Ajnin)
+				tegratMap.put(damager.getUniqueId(), player.getUniqueId());
+		}
+	}
+
+	@EventHandler
+	private void onPlayerToggleSneak(PlayerToggleSneakEvent event) {
+		if (event.isSneaking())
+			return;
+
+		Player player = event.getPlayer();
+		if (VapoxPvP.getKit(player) instanceof Ajnin) {
+			if (!VapoxPvP.hasKitCooldown(player)) {
+				if (tegratMap.containsKey(player.getUniqueId())) {
+					Player target = Bukkit.getPlayer(tegratMap.get(player.getUniqueId()));
+					if (target != null && !target.isDead() && player.getLocation().distance(target.getLocation()) <= 35D
+							&& VapoxPvP.hasKit(target)) {
+						VapoxPvP.addKitCooldown(player, 10);
+						player.sendMessage("§a§l[" + this.getName().toUpperCase() + "] §fVocê puxou o jogador §a"
+								+ target.getName() + "§faté você!");
+
+						target.teleport(player.getLocation());
+						target.sendMessage("§a§l[" + this.getName().toUpperCase() + "] §e" + player.getName()
+								+ " §fteleportou você até ele!");
+					} else
+						player.sendMessage(
+								"§c§l[" + this.getName().toUpperCase() + "] §cO seu ninja não encontrou ninguém!");
+				} else
+					player.sendMessage("§c§l[" + this.getName().toUpperCase() + "] §cVocê não atingiu ninguém!");
+			} else
+				player.sendMessage("§c§l[" + this.getName().toUpperCase() + "] §fAguarde §c"
+						+ VapoxPvP.getFormattedKitCooldown(player) + " §fpara usar novamente!");
+		}
+	}
+
+	@EventHandler
+	private void onPlayerDeath(PlayerDeathEvent event) {
+		tegratMap.remove(event.getEntity().getUniqueId());
 	}
 }
