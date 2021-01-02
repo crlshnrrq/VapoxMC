245d82a521265b3fdaf271c48560672ba01b7d25
crlshnrrq <caarlosheenrique01@outlook.com>
2021-01-02 10:31:58 -0300

    Added Viking ability

--- src/br/com/vapoxmc/kitpvp/kit/Viking.java
+++ src/br/com/vapoxmc/kitpvp/kit/Viking.java
@@ -2,11 +2,15 @@ package br.com.vapoxmc.kitpvp.kit;
 
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.inventory.PlayerInventory;
 
+import br.com.vapoxmc.kitpvp.VapoxPvP;
 import br.com.vapoxmc.kitpvp.utils.Stack;
 
-public final class Viking extends Kit {
+public final class Viking extends Kit implements Listener {
 
 	public Viking() {
 		super("Viking", "De mais dano com seu machado!", new Stack(Material.STONE_AXE), new Stack(Material.STONE_AXE)
@@ -19,5 +23,14 @@ public final class Viking extends Kit {
 		super.giveItems(player);
 		inv.setItem(0, new Stack(Material.STONE_AXE).display("§aMachado §7(§f§lViking§7)")
 				.lore("§7Clique com o §6esquerdo §7para atacar um oponente."));
+	}
+
+	@EventHandler
+	private void onEntityDamageByEntity(EntityDamageByEntityEvent event) {
+		if (event.getEntity() instanceof Player && event.getDamager() instanceof Player) {
+			Player damager = (Player) event.getDamager();
+			if (VapoxPvP.getKit(damager) instanceof Viking && damager.getItemInHand().getType().name().contains("_AXE"))
+				event.setDamage(event.getDamage() + 2D);
+		}
 	}
 }
