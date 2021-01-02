b2cc643f184030e844550c80d598058740451155
crlshnrrq <caarlosheenrique01@outlook.com>
2021-01-01 23:05:59 -0300

    Adicionado classe com utilidades relacionadas ao Servidor

---
+++ src/br/com/vapoxmc/kitpvp/utils/VapoxUtils.java
@@ -0,0 +1,12 @@
+package br.com.vapoxmc.kitpvp.utils;
+
+public final class VapoxUtils {
+
+	public static int getRandomPoints() {
+		return (int) (Math.random() * -20 + 30);
+	}
+
+	public static int getRandomCoins() {
+		return (int) (Math.random() * -20 + 30);
+	}
+}
