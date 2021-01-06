e7738197a876830f3d3f31c2450bd6e8387de58b
crlshnrrq <caarlosheenrique01@outlook.com>
2021-01-05 22:49:21 -0300

    Adicionado motd e convite do discord

--- src/br/com/vapoxmc/kitpvp/utils/Strings.java
+++ src/br/com/vapoxmc/kitpvp/utils/Strings.java
@@ -9,4 +9,12 @@ public final class Strings {
 	public static String getPrefix() {
 		return getName() + " §7»";
 	}
+
+	public static String getMotd() {
+		return "                         §f§lVapox§4§lMC\n     §aVenha treinar e se divertir com seus amigos!";
+	}
+
+	public static String getDiscord() {
+		return "https://discord.gg/KgfUdYH";
+	}
 }
