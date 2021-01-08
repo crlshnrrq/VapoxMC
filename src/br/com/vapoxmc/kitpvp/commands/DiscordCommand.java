0d81d0c47097d5c51d2a551996c3207fb2b054f9
crlshnrrq <caarlosheenrique01@outlook.com>
2021-01-07 21:10:09 -0300

    Adicionado comando /discord

---
+++ src/br/com/vapoxmc/kitpvp/commands/DiscordCommand.java
@@ -0,0 +1,29 @@
+package br.com.vapoxmc.kitpvp.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import br.com.vapoxmc.kitpvp.utils.Strings;
+import net.md_5.bungee.api.chat.ClickEvent;
+import net.md_5.bungee.api.chat.TextComponent;
+import net.md_5.bungee.api.chat.ClickEvent.Action;
+
+public final class DiscordCommand implements CommandExecutor {
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+		if (sender instanceof Player) {
+			Player player = (Player) sender;
+			player.sendMessage(" ");
+			TextComponent discord = new TextComponent("§fClique §e§lAQUI §fpara acessar o §5§ldiscord§f.");
+			discord.setClickEvent(new ClickEvent(Action.OPEN_URL, Strings.getDiscord()));
+			player.spigot().sendMessage(discord);
+			player.sendMessage("§fOu, se preferir, o §alink direto §fé: §a" + Strings.getDiscord());
+			player.sendMessage(" ");
+		} else
+			sender.sendMessage("§cVocê não pode executar este comando!");
+		return true;
+	}
+}
