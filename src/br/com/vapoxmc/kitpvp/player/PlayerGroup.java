11eef384e9ff88bc925d0bdc7cdb8df7103e6ae4
crlshnrrq <caarlosheenrique01@outlook.com>
2021-01-08 18:59:35 -0300

    Adicionado método para retornar o nome colorido do cargo

--- src/br/com/vapoxmc/kitpvp/player/PlayerGroup.java
+++ src/br/com/vapoxmc/kitpvp/player/PlayerGroup.java
@@ -4,23 +4,16 @@ import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 
 public enum PlayerGroup {
-	DONO("Dono", "ciphen.tag.dono", ChatColor.DARK_RED),
-	DIRETOR("Diretor", "ciphen.tag.diretor", ChatColor.DARK_RED),
-	COORD("Coord", "ciphen.tag.coord", ChatColor.DARK_AQUA),
-	ADMIN("Admin", "ciphen.tag.admin", ChatColor.RED),
-	GERENTE("Gerente", "ciphen.tag.gerente", ChatColor.RED),
-	MODGC("ModGC", "ciphen.tag.modgc", ChatColor.DARK_PURPLE),
-	MOD("Mod", "ciphen.tag.mod", ChatColor.DARK_PURPLE),
-	TRIAL("Trial", "ciphen.tag.trial", ChatColor.LIGHT_PURPLE),
+	DONO("Dono", "ciphen.tag.dono", ChatColor.DARK_RED), DIRETOR("Diretor", "ciphen.tag.diretor", ChatColor.DARK_RED),
+	COORD("Coord", "ciphen.tag.coord", ChatColor.DARK_AQUA), ADMIN("Admin", "ciphen.tag.admin", ChatColor.RED),
+	GERENTE("Gerente", "ciphen.tag.gerente", ChatColor.RED), MODGC("ModGC", "ciphen.tag.modgc", ChatColor.DARK_PURPLE),
+	MOD("Mod", "ciphen.tag.mod", ChatColor.DARK_PURPLE), TRIAL("Trial", "ciphen.tag.trial", ChatColor.LIGHT_PURPLE),
 	AJUDANTE("Ajudante", "ciphen.tag.ajudante", ChatColor.BLUE),
 	BUILDER("Builder", "ciphen.tag.builder", ChatColor.DARK_GREEN),
 	YOUTUBERPLUS("YT+", "ciphen.tag.youtuberplus", ChatColor.DARK_AQUA),
-	YOUTUBER("YT", "ciphen.tag.youtuber", ChatColor.AQUA),
-	BETA("Beta", "ciphen.tag.beta", ChatColor.YELLOW),
-	HYPER("Hyper", "ciphen.tag.hyper", ChatColor.AQUA),
-	PRO("Pro", "ciphen.tag.pro", ChatColor.GOLD),
-	MVP("MvP", "ciphen.tag.mvp", ChatColor.BLUE),
-	VIP("VIP", "ciphen.tag.vip", ChatColor.GREEN),
+	YOUTUBER("YT", "ciphen.tag.youtuber", ChatColor.AQUA), BETA("Beta", "ciphen.tag.beta", ChatColor.YELLOW),
+	HYPER("Hyper", "ciphen.tag.hyper", ChatColor.AQUA), PRO("Pro", "ciphen.tag.pro", ChatColor.GOLD),
+	MVP("MvP", "ciphen.tag.mvp", ChatColor.BLUE), VIP("VIP", "ciphen.tag.vip", ChatColor.GREEN),
 	MEMBRO("Membro", "ciphen.tag.membro", ChatColor.GRAY);
 
 	private final String name, permission;
@@ -42,6 +35,10 @@ public enum PlayerGroup {
 
 	public ChatColor getColor() {
 		return this.color;
+	}
+
+	public String getColoredName() {
+		return this.getColor() + this.getName();
 	}
 
 	public String getBoldColoredName() {
