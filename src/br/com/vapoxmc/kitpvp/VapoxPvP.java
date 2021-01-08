d4e6612c871fa46e16112062680ec61f921b0533
crlshnrrq <caarlosheenrique01@outlook.com>
2021-01-08 19:20:08 -0300

    Adicionado comando /lastlogin

--- src/br/com/vapoxmc/kitpvp/VapoxPvP.java
+++ src/br/com/vapoxmc/kitpvp/VapoxPvP.java
@@ -26,6 +26,7 @@ import br.com.vapoxmc.kitpvp.commands.HeadCommand;
 import br.com.vapoxmc.kitpvp.commands.InfoCommand;
 import br.com.vapoxmc.kitpvp.commands.InvseeCommand;
 import br.com.vapoxmc.kitpvp.commands.KitCommand;
+import br.com.vapoxmc.kitpvp.commands.LastLoginCommand;
 import br.com.vapoxmc.kitpvp.commands.OnlineCommand;
 import br.com.vapoxmc.kitpvp.commands.PingCommand;
 import br.com.vapoxmc.kitpvp.commands.RankCommand;
@@ -369,6 +370,7 @@ public final class VapoxPvP extends JavaPlugin {
 		this.getCommand("infocommand").setExecutor(new InfoCommand());
 		this.getCommand("invsee").setExecutor(new InvseeCommand());
 		this.getCommand("kit").setExecutor(new KitCommand());
+		this.getCommand("lastlogin").setExecutor(new LastLoginCommand());
 		this.getCommand("online").setExecutor(new OnlineCommand());
 		this.getCommand("ping").setExecutor(new PingCommand());
 		this.getCommand("rank").setExecutor(new RankCommand());
---
+++ src/br/com/vapoxmc/kitpvp/commands/LastLoginCommand.java
@@ -0,0 +1,40 @@
+package br.com.vapoxmc.kitpvp.commands;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+@SuppressWarnings("deprecation")
+public final class LastLoginCommand implements CommandExecutor {
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+		if (sender instanceof Player) {
+			Player player = (Player) sender;
+			if (player.hasPermission("ciphen.comandos.lastlogin")) {
+				if (args.length > 0) {
+					OfflinePlayer target = Bukkit.getOfflinePlayer(args[0]);
+					if (target.hasPlayedBefore()) {
+						Date date = new Date(target.getLastPlayed());
+						player.sendMessage("§c§l[LastLogin] §fO último login de §c" + target.getName() + " §ffoi em: "
+								+ new SimpleDateFormat("dd/MM/yyyy").format(date) + " §làs §f"
+								+ new SimpleDateFormat("HH:mm:ss").format(date));
+					} else {
+						player.sendMessage(
+								"§c§l[LastLogin] §fNão foi possível encontrar esse jogador no banco de dados. §7(hpb_01)");
+					}
+				} else
+					player.sendMessage("§cComando incorreto, utilize /" + label + " [Jogador]");
+			} else
+				player.sendMessage("§cÉ necessário ser [ADMIN] ou superior para executar este comando!");
+		} else
+			sender.sendMessage("§cVocê não pode executar este comando!");
+		return true;
+	}
+}
