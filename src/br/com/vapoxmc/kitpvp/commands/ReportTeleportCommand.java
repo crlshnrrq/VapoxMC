7de7f43da78288c9aeef45763eb67c519ed62f00
crlshnrrq <caarlosheenrique01@outlook.com>
2021-01-09 15:09:28 -0300

    Corrigindo bug de condição invertida

--- src/br/com/vapoxmc/kitpvp/commands/ReportTeleportCommand.java
+++ src/br/com/vapoxmc/kitpvp/commands/ReportTeleportCommand.java
@@ -14,7 +14,7 @@ public final class ReportTeleportCommand implements CommandExecutor {
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 		if (sender instanceof Player) {
 			Player player = (Player) sender;
-			if (!player.hasPermission("ciphen.comandos.tp")) {
+			if (player.hasPermission("ciphen.comandos.tp")) {
 				if (args.length > 0) {
 					Player target = Bukkit.getPlayer(args[0]);
 					if (target != null) {
