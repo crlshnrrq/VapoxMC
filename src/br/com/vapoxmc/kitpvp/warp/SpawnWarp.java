9123a7e59453264c75722eb2fae532ab94453b45
crlshnrrq <caarlosheenrique01@outlook.com>
2021-01-09 13:51:53 -0300

    Bloqueando o drop de itens no spawn

--- src/br/com/vapoxmc/kitpvp/warp/SpawnWarp.java
+++ src/br/com/vapoxmc/kitpvp/warp/SpawnWarp.java
@@ -3,9 +3,12 @@ package br.com.vapoxmc.kitpvp.warp;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerDropItemEvent;
 import org.bukkit.inventory.PlayerInventory;
 
+import br.com.vapoxmc.kitpvp.VapoxPvP;
 import br.com.vapoxmc.kitpvp.gui.SeusKitsGUI;
 import br.com.vapoxmc.kitpvp.gui.ShopGUI;
 import br.com.vapoxmc.kitpvp.gui.WarpsGUI;
@@ -25,5 +28,12 @@ public final class SpawnWarp extends Warp implements Listener {
 		inv.setItem(2, WarpsGUI.ICON);
 		inv.setItem(4, SeusKitsGUI.ICON);
 		inv.setItem(6, ShopGUI.ICON);
+	}
+
+	@EventHandler
+	private void onPlayerDropItem(PlayerDropItemEvent event) {
+		Player player = event.getPlayer();
+		if (VapoxPvP.getWarp(player) instanceof SpawnWarp)
+			event.setCancelled(true);
 	}
 }
