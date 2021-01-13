b75cb7e74117ac5b6611f6ebf224e7630dc349d6
crlshnrrq <caarlosheenrique01@outlook.com>
2021-01-12 21:34:45 -0300

    Melhorando o combate e os locais

--- src/br/com/vapoxmc/kitpvp/listeners/CombatLogListeners.java
+++ src/br/com/vapoxmc/kitpvp/listeners/CombatLogListeners.java
@@ -14,7 +14,10 @@ import org.bukkit.event.player.PlayerQuitEvent;
 import br.com.vapoxmc.kitpvp.VapoxPvP;
 import br.com.vapoxmc.kitpvp.player.PlayerAccount;
 import br.com.vapoxmc.kitpvp.utils.VapoxUtils;
-import br.com.vapoxmc.kitpvp.warp.LavaChallengeWarp;
+import br.com.vapoxmc.kitpvp.warp.EventoWarp;
+import br.com.vapoxmc.kitpvp.warp.FPSWarp;
+import br.com.vapoxmc.kitpvp.warp.FishermanWarp;
+import br.com.vapoxmc.kitpvp.warp.PotPvPWarp;
 import br.com.vapoxmc.kitpvp.warp.UMvUMWarp;
 
 public final class CombatLogListeners implements Listener {
@@ -55,10 +58,16 @@ public final class CombatLogListeners implements Listener {
 	private void onEntityDamageByEntity(EntityDamageByEntityEvent event) {
 		if (event.getEntity() instanceof Player && event.getDamager() instanceof Player) {
 			Player player = (Player) event.getEntity(), damager = (Player) event.getDamager();
-			if (VapoxPvP.hasKit(player) && VapoxPvP.hasKit(damager) && !(VapoxPvP.getWarp(damager) instanceof UMvUMWarp)
-					&& !(VapoxPvP.getWarp(player) instanceof UMvUMWarp)
-					&& !(VapoxPvP.getWarp(damager) instanceof LavaChallengeWarp
-							&& !(VapoxPvP.getWarp(player) instanceof LavaChallengeWarp))) {
+			if ((VapoxPvP.hasKit(player) && VapoxPvP.hasKit(damager))
+					|| (VapoxPvP.getWarp(player) instanceof FPSWarp && VapoxPvP.getWarp(damager) instanceof FPSWarp)
+					|| (VapoxPvP.getWarp(player) instanceof EventoWarp
+							&& VapoxPvP.getWarp(damager) instanceof EventoWarp)
+					|| (VapoxPvP.getWarp(player) instanceof FishermanWarp
+							&& VapoxPvP.getWarp(damager) instanceof FishermanWarp)
+					|| (VapoxPvP.getWarp(player) instanceof PotPvPWarp
+							&& VapoxPvP.getWarp(damager) instanceof PotPvPWarp)
+					|| (VapoxPvP.getWarp(player) instanceof UMvUMWarp
+							&& VapoxPvP.getWarp(damager) instanceof UMvUMWarp)) {
 				if (!VapoxPvP.isInCombat(player))
 					VapoxPvP.addCombat(player, damager);
 				else
