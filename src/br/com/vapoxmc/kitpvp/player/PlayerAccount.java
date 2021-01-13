90b3834d5587ba8bf3cb0db08be3d65b925be717
crlshnrrq <caarlosheenrique01@outlook.com>
2021-01-12 21:21:31 -0300

    Atualizando o método dos status

--- src/br/com/vapoxmc/kitpvp/player/PlayerAccount.java
+++ src/br/com/vapoxmc/kitpvp/player/PlayerAccount.java
@@ -1,69 +1,48 @@
 package br.com.vapoxmc.kitpvp.player;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.io.File;
+import java.io.IOException;
 
-import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 
-import br.com.vapoxmc.kitpvp.utils.MySQL;
+import br.com.vapoxmc.kitpvp.VapoxPvP;
 
 public final class PlayerAccount {
 
-	private static MySQL mysql;
+	private static File file;
+	private static YamlConfiguration config;
 
 	public static void createConnection() {
-		try {
-			mysql = new MySQL("127.0.0.1", 3306, "vapoxmc", "root", "");
-		} catch (SQLException ex) {
-			Bukkit.getConsoleSender().sendMessage("§cNão foi possível conectar ao Banco de Dados. (SQLException)");
-		}
-
-		try {
-			mysql.createTable("geral",
-					"(Nickname VARCHAR(16), Moedas INT(13), Pontos INT(13), KillStreak INT(13), Abates INT(13), Mortes INT(13))");
-		} catch (SQLException ex) {
-			ex.printStackTrace();
-			Bukkit.getConsoleSender().sendMessage("§cNão foi possível criar a Tabela \"geral\". (SQLException)");
-		}
-
-		try {
-			mysql.createTable("umvum", "(Nickname VARCHAR(16), WinStreak INT(13), Vitorias INT(13), Derrotas INT(13))");
-		} catch (SQLException ex) {
-			ex.printStackTrace();
-			Bukkit.getConsoleSender().sendMessage("§cNão foi possível criar a Tabela \"umvum\". (SQLException)");
-		}
-
-		try {
-			mysql.createTable("sumo", "(Nickname VARCHAR(16), WinStreak INT(13), Vitorias INT(13), Derrotas INT(13))");
-		} catch (SQLException ex) {
-			ex.printStackTrace();
-			Bukkit.getConsoleSender().sendMessage("§cNão foi possível criar a Tabela \"sumo\". (SQLException)");
-		}
+		file = new File(VapoxPvP.getInstance().getDataFolder(), "status.yml");
+		if (!file.exists())
+			file.getParentFile().mkdirs();
+		config = YamlConfiguration.loadConfiguration(file);
 	}
 
 	public static boolean existAccount(String nickname) {
-		try {
-			return mysql.selectFrom("geral", "Nickname='" + nickname + "'").next();
-		} catch (SQLException ex) {
-			ex.printStackTrace();
-		}
-		return false;
+		return config.contains("status." + nickname);
 	}
 
 	public static void createAccount(Player player) {
 		if (!existAccount(player.getName())) {
 			try {
-				mysql.insertInto("geral", "(Nickname, Moedas, Pontos, Abates, Mortes, KillStreak)",
-						"('" + player.getName() + "', '0', '0', '0', '0', '0')");
-				mysql.insertInto("umvum", "(Nickname, Vitorias, Derrotas, WinStreak)",
-						"('" + player.getName() + "', '0', '0', '0')");
-				mysql.insertInto("sumo", "(Nickname, Vitorias, Derrotas, WinStreak)",
-						"('" + player.getName() + "', '0', '0', '0')");
-			} catch (SQLException ex) {
+				config.set("status." + player.getName() + ".geral.moedas", 0);
+				config.set("status." + player.getName() + ".geral.pontos", 0);
+				config.set("status." + player.getName() + ".geral.abates", 0);
+				config.set("status." + player.getName() + ".geral.mortes", 0);
+				config.set("status." + player.getName() + ".geral.killstreak", 0);
+
+				config.set("status." + player.getName() + ".1v1.vitorias", 0);
+				config.set("status." + player.getName() + ".1v1.derrotas", 0);
+				config.set("status." + player.getName() + ".1v1.winstreak", 0);
+
+				config.set("status." + player.getName() + ".sumo.vitorias", 0);
+				config.set("status." + player.getName() + ".sumo.derrotas", 0);
+				config.set("status." + player.getName() + ".sumo.winstreak", 0);
+				config.save(file);
+			} catch (IOException ex) {
 				ex.printStackTrace();
-				Bukkit.getConsoleSender()
-						.sendMessage("§cNão foi possível criar a Conta de " + player.getName() + ". (SQLException)");
 			}
 		}
 	}
@@ -75,20 +54,14 @@ public final class PlayerAccount {
 	public static class Geral {
 
 		public int getMoedas(Player player) {
-			try {
-				ResultSet set = mysql.selectFrom("geral", "Nickname='" + player.getName() + "'");
-				if (set.next())
-					return set.getInt("Moedas");
-			} catch (SQLException ex) {
-				ex.printStackTrace();
-			}
-			return 0;
+			return config.getInt("status." + player.getName() + ".geral.moedas", 0);
 		}
 
 		public Geral setMoedas(Player player, int amount) {
 			try {
-				mysql.update("geral", "Nickname='" + player.getName() + "'", "Moedas='" + amount + "'");
-			} catch (SQLException ex) {
+				config.set("status." + player.getName() + ".geral.moedas", amount);
+				config.save(file);
+			} catch (IOException ex) {
 				ex.printStackTrace();
 			}
 			return getGeral();
@@ -106,20 +79,14 @@ public final class PlayerAccount {
 		}
 
 		public int getPontos(Player player) {
-			try {
-				ResultSet set = mysql.selectFrom("geral", "Nickname='" + player.getName() + "'");
-				if (set.next())
-					return set.getInt("Pontos");
-			} catch (SQLException ex) {
-				ex.printStackTrace();
-			}
-			return 0;
+			return config.getInt("status." + player.getName() + ".geral.pontos", 0);
 		}
 
 		public Geral setPontos(Player player, int amount) {
 			try {
-				mysql.update("geral", "Nickname='" + player.getName() + "'", "Pontos='" + amount + "'");
-			} catch (SQLException ex) {
+				config.set("status." + player.getName() + ".geral.pontos", amount);
+				config.save(file);
+			} catch (IOException ex) {
 				ex.printStackTrace();
 			}
 			return getGeral();
@@ -131,20 +98,14 @@ public final class PlayerAccount {
 		}
 
 		public int getKillStreak(Player player) {
-			try {
-				ResultSet set = mysql.selectFrom("geral", "Nickname='" + player.getName() + "'");
-				if (set.next())
-					return set.getInt("KillStreak");
-			} catch (SQLException ex) {
-				ex.printStackTrace();
-			}
-			return 0;
+			return config.getInt("status." + player.getName() + ".geral.killstreak", 0);
 		}
 
 		public Geral setKillStreak(Player player, int amount) {
 			try {
-				mysql.update("geral", "Nickname='" + player.getName() + "'", "KillStreak='" + amount + "'");
-			} catch (SQLException ex) {
+				config.set("status." + player.getName() + ".geral.killstreak", amount);
+				config.save(file);
+			} catch (IOException ex) {
 				ex.printStackTrace();
 			}
 			return getGeral();
@@ -156,20 +117,14 @@ public final class PlayerAccount {
 		}
 
 		public int getAbates(Player player) {
-			try {
-				ResultSet set = mysql.selectFrom("geral", "Nickname='" + player.getName() + "'");
-				if (set.next())
-					return set.getInt("Abates");
-			} catch (SQLException ex) {
-				ex.printStackTrace();
-			}
-			return 0;
+			return config.getInt("status." + player.getName() + ".geral.abates", 0);
 		}
 
 		public Geral setAbates(Player player, int amount) {
 			try {
-				mysql.update("geral", "Nickname='" + player.getName() + "'", "Abates='" + amount + "'");
-			} catch (SQLException ex) {
+				config.set("status." + player.getName() + ".geral.abates", amount);
+				config.save(file);
+			} catch (IOException ex) {
 				ex.printStackTrace();
 			}
 			return getGeral();
@@ -185,20 +140,14 @@ public final class PlayerAccount {
 		}
 
 		public int getMortes(Player player) {
-			try {
-				ResultSet set = mysql.selectFrom("geral", "Nickname='" + player.getName() + "'");
-				if (set.next())
-					return set.getInt("Mortes");
-			} catch (SQLException ex) {
-				ex.printStackTrace();
-			}
-			return 0;
+			return config.getInt("status." + player.getName() + ".geral.mortes", 0);
 		}
 
 		public Geral setMortes(Player player, int amount) {
 			try {
-				mysql.update("geral", "Nickname='" + player.getName() + "'", "Mortes='" + amount + "'");
-			} catch (SQLException ex) {
+				config.set("status." + player.getName() + ".geral.mortes", amount);
+				config.save(file);
+			} catch (IOException ex) {
 				ex.printStackTrace();
 			}
 			return getGeral();
@@ -221,20 +170,14 @@ public final class PlayerAccount {
 	public static class UMvUM {
 
 		public int getWinStreak(Player player) {
-			try {
-				ResultSet set = mysql.selectFrom("umvum", "Nickname='" + player.getName() + "'");
-				if (set.next())
-					return set.getInt("WinStreak");
-			} catch (SQLException ex) {
-				ex.printStackTrace();
-			}
-			return 0;
+			return config.getInt("status." + player.getName() + ".1v1.winstreak");
 		}
 
 		public UMvUM setWinStreak(Player player, int amount) {
 			try {
-				mysql.update("umvum", "Nickname='" + player.getName() + "'", "WinStreak='" + amount + "'");
-			} catch (SQLException ex) {
+				config.set("status." + player.getName() + ".1v1.winstreak", amount);
+				config.save(file);
+			} catch (IOException ex) {
 				ex.printStackTrace();
 			}
 			return get1v1();
@@ -246,20 +189,14 @@ public final class PlayerAccount {
 		}
 
 		public int getVitorias(Player player) {
-			try {
-				ResultSet set = mysql.selectFrom("umvum", "Nickname='" + player.getName() + "'");
-				if (set.next())
-					return set.getInt("Vitorias");
-			} catch (SQLException ex) {
-				ex.printStackTrace();
-			}
-			return 0;
+			return config.getInt("status." + player.getName() + ".1v1.vitorias");
 		}
 
 		public UMvUM setVitorias(Player player, int amount) {
 			try {
-				mysql.update("umvum", "Nickname='" + player.getName() + "'", "Vitorias='" + amount + "'");
-			} catch (SQLException ex) {
+				config.set("status." + player.getName() + ".1v1.vitorias", amount);
+				config.save(file);
+			} catch (IOException ex) {
 				ex.printStackTrace();
 			}
 			return get1v1();
@@ -275,20 +212,14 @@ public final class PlayerAccount {
 		}
 
 		public int getDerrotas(Player player) {
-			try {
-				ResultSet set = mysql.selectFrom("umvum", "Nickname='" + player.getName() + "'");
-				if (set.next())
-					return set.getInt("Derrotas");
-			} catch (SQLException ex) {
-				ex.printStackTrace();
-			}
-			return 0;
+			return config.getInt("status." + player.getName() + ".1v1.derrotas", 0);
 		}
 
 		public UMvUM setDerrotas(Player player, int amount) {
 			try {
-				mysql.update("umvum", "Nickname='" + player.getName() + "'", "Derrotas='" + amount + "'");
-			} catch (SQLException ex) {
+				config.set("status." + player.getName() + ".1v1.derrotas", amount);
+				config.save(file);
+			} catch (IOException ex) {
 				ex.printStackTrace();
 			}
 			return get1v1();
@@ -311,20 +242,14 @@ public final class PlayerAccount {
 	public static class Sumo {
 
 		public int getWinStreak(Player player) {
-			try {
-				ResultSet set = mysql.selectFrom("sumo", "Nickname='" + player.getName() + "'");
-				if (set.next())
-					return set.getInt("WinStreak");
-			} catch (SQLException ex) {
-				ex.printStackTrace();
-			}
-			return 0;
+			return config.getInt("status." + player.getName() + ".sumo.winstreak", 0);
 		}
 
 		public Sumo setWinStreak(Player player, int amount) {
 			try {
-				mysql.update("sumo", "Nickname='" + player.getName() + "'", "WinStreak='" + amount + "'");
-			} catch (SQLException ex) {
+				config.set("status." + player.getName() + ".sumo.winstreak", amount);
+				config.save(file);
+			} catch (IOException ex) {
 				ex.printStackTrace();
 			}
 			return getSumo();
@@ -336,20 +261,14 @@ public final class PlayerAccount {
 		}
 
 		public int getVitorias(Player player) {
-			try {
-				ResultSet set = mysql.selectFrom("sumo", "Nickname='" + player.getName() + "'");
-				if (set.next())
-					return set.getInt("Vitorias");
-			} catch (SQLException ex) {
-				ex.printStackTrace();
-			}
-			return 0;
+			return config.getInt("status." + player.getName() + ".sumo.vitorias", 0);
 		}
 
 		public Sumo setVitorias(Player player, int amount) {
 			try {
-				mysql.update("sumo", "Nickname='" + player.getName() + "'", "Vitorias='" + amount + "'");
-			} catch (SQLException ex) {
+				config.set("status." + player.getName() + ".sumo.vitorias", amount);
+				config.save(file);
+			} catch (IOException ex) {
 				ex.printStackTrace();
 			}
 			return getSumo();
@@ -365,20 +284,14 @@ public final class PlayerAccount {
 		}
 
 		public int getDerrotas(Player player) {
-			try {
-				ResultSet set = mysql.selectFrom("sumo", "Nickname='" + player.getName() + "'");
-				if (set.next())
-					return set.getInt("Derrotas");
-			} catch (SQLException ex) {
-				ex.printStackTrace();
-			}
-			return 0;
+			return config.getInt("status." + player.getName() + ".sumo.derrotas", 0);
 		}
 
 		public Sumo setDerrotas(Player player, int amount) {
 			try {
-				mysql.update("sumo", "Nickname='" + player.getName() + "'", "Derrotas='" + amount + "'");
-			} catch (SQLException ex) {
+				config.set("status." + player.getName() + ".sumo.derrotas", amount);
+				config.save(file);
+			} catch (IOException ex) {
 				ex.printStackTrace();
 			}
 			return getSumo();
